b9d97c059f13bf362acd7baeac7e939b6896973a
Tji Fung <tjifung@Monkefit-2.local>
2026-02-13 13:30:18 +0700

    full inter font setup

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,35 @@
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-<link
-  href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-  rel="stylesheet"
-/>
-
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Fonts - Inter */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+
+        {/* Smooth font rendering */}
+        <style>{`
+          html {
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+          }
+          body {
+            margin: 0;
+            font-family: 'Inter', -apple-system, BlinkMacSystemFont, system-ui, sans-serif;
+            background-color: #0b1220;
+            color: #f9fafb;
+          }
+        `}</style>
+      </Head>
       <body>
         <Main />
         <NextScript />
@@ -18,3 +37,4 @@ export default function Document() {
     </Html>
   );
 }
+
